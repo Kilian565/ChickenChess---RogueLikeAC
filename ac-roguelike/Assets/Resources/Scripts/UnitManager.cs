20df395d2ac97a26538643c810a382f527f44c9c
Hauke <hauke.hausmann@edu.bib.de>
2019-12-02 14:54:01 +0100

    Version 0.2.5

--- ac-roguelike/Assets/Resources/Scripts/UnitManager.cs
+++ ac-roguelike/Assets/Resources/Scripts/UnitManager.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class UnitManager : MonoBehaviour
 {
-    public static List<GameObject> tier1Units;
-    public static List<GameObject> tier2Units;
-    public static List<GameObject> tier3Units;
+    GameObject platzhalter_1Pref;
+    GameObject platzhalter_2Pref;
+    GameObject platzhalter_3Pref;
+    GameObject platzhalter_4Pref;
+    GameObject platzhalter_5Pref;
 
     private void Awake()
     {
-        
+        platzhalter_1Pref = Resources.Load<GameObject>("PlatzhalterPrefabs/Platzhalter1");
+        platzhalter_2Pref = Resources.Load<GameObject>("PlatzhalterPrefabs/Platzhalter2");
+        platzhalter_3Pref = Resources.Load<GameObject>("PlatzhalterPrefabs/Platzhalter3");
+        platzhalter_4Pref = Resources.Load<GameObject>("PlatzhalterPrefabs/Platzhalter4");
+        platzhalter_5Pref = Resources.Load<GameObject>("PlatzhalterPrefabs/Platzhalter5");
     }
 
     void Start ()
     {
-        
+
 	}
 	
 	void Update ()
